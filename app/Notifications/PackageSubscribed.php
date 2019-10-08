2c469238837208e0e6e848a9eabc933071880267
cloudtechy <conyekelu@yahoo.com>
2019-10-08 19:05:51 +0100

    packageSubscribed notification fix

--- app/Notifications/PackageSubscribed.php
+++ app/Notifications/PackageSubscribed.php
@@ -44,8 +44,7 @@ class PackageSubscribed extends Notification implements ShouldQueue {
 			->subject('Successful Package Subscription')
 			->line('Your subscription was successful and your active account credited with $' . $package->deposit)
 			->action('Goto Dashboard', url($dashboardPath))
-			->line('Thank you for investing with us')
-			->bcc(config('frontend.admin_email'),config('frontend.admin_name'));
+			->line('Thank you for investing with us');
 	}
 
 	/**
