571fe69d76f71f426533c69cd15ebb633d79f640
cloudtechy <conyekelu@yahoo.com>
2020-07-07 09:22:18 +0100

    Reset mail Notification will be sent on realtime

--- app/Notifications/MailResetPasswordNotification.php
+++ app/Notifications/MailResetPasswordNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class MailResetPasswordNotification extends ResetPassword implements ShouldQueue {
+class MailResetPasswordNotification extends ResetPassword {
 	use Queueable;
 	/**
 	 * Create a new notification instance.
